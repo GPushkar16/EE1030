1e8f6aa8e2edafa8b8241da2affdce428a8c1343
GPushkar16 <ai24btech11012@iith.ac.in>
2024-08-18 09:19:38 +0530

    edit

---
+++ assignment-2/codes/parallelogram.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+A = np.array([3, 3])
+B = np.array([6, 4])
+C = np.array([8, 7])
+D = np.array([5, 6])
+
+plt.figure(figsize=(8, 6))
+
+plt.plot([A[0], B[0]], [A[1], B[1]])
+plt.plot([B[0], C[0]], [B[1], C[1]])
+plt.plot([C[0], D[0]], [C[1], D[1]])
+plt.plot([A[0], D[0]], [A[1], D[1]])
+
+plt.text(A[0], A[1], 'A(3, 3)')
+plt.text(B[0], B[1], 'B(6,4)')
+plt.text(C[0], C[1], 'C(8, 7)')
+plt.text(D[0], D[1], 'D(5, 6)')
+
+plt.show()
