45ae37e50c2a4a0cc498e7ba87990f36f4a8c6f6
GPushkar16 <ai24btech11012@iith.ac.in>
2024-09-09 22:19:27 +0530

    edit

--- assignemnt-3/codes/plot.py
+++ assignemnt-3/codes/plot.py
@@ -1,14 +1,24 @@
+import sys                                  
+sys.path.insert(0, '/home/matgeo/codes/CoordGeo/line')    
 import numpy as np
+import numpy.linalg as LA
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from funcs import line_gen
+import params
 
 A = np.array(([4 , 4]))
 B = np.array(([1, 0]))
 C = np.array(([4, -4]))
 
+x_AB = line_gen(A,B)
+x_BC = line_gen(B,C)
+
 plt.figure(figsize=(8, 6))
 
-plt.plot([A[0], B[0]], [A[1], B[1]])
-plt.plot([B[0],C[0]], [B[1], C[1]])
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
 
 
 plt.text(A[0], A[1], 'A(4,4)')
