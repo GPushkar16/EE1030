5d7dbb9d4216db98e4bd5134563f1d125f1f6100
GPushkar16 <ai24btech11012@iith.ac.in>
2024-09-22 11:11:53 +0530

    edit

---
+++ assignment-4/codes/plot1.py
@@ -0,0 +1,36 @@
+import sys                                  
+sys.path.insert(0, '/home/matgeo/codes/CoordGeo/line')    
+import numpy as np
+import numpy.linalg as LA
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from funcs import line_gen
+import params
+
+A = np.array(([0.34 , 0.34]))
+B = np.array(([0, 0]))
+C = np.array(([6, 0]))
+
+x_AB = line_gen(A,B)
+x_BC = line_gen(B,C)
+x_AC = line_gen(A,C)
+
+plt.figure(figsize=(8, 6))
+
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_AC[0,:],x_AC[1,:],label='$AC$')
+
+
+plt.text(A[0], A[1], 'A(0.34,0.34)')
+plt.text(B[0], B[1], 'B(0,0)')
+plt.text(C[0], C[1], 'C(6,0)')
+plt.xlabel('X-coordinate')
+plt.ylabel('Y-coordinate')
+plt.title('Plot of the triangle ABC such that difference between AB and AC is 5.2 cm')
+plt.legend()
+plt.grid(True)
+
+
+plt.savefig('plot1.png')
